f5a1c29ceb3dc5de75ba9ba657db4fe42c36fbf9
HakuAsterisk <Jetro.tolli@gmail.com>
2026-03-16 17:25:03 +0200

    3.1 - 3.6 submissions

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const app = express();
+let data = require("./data.json");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("<h1>Phonebook!</h1>");
+});
+
+// info
+
+app.get("/info", (req, res) => {
+  const date = new Date();
+  res.send(`
+    <p>Phonebook has info for ${data.length} people</p>
+    <p>${date}</p>
+    `);
+});
+
+// get
+
+app.get("/api/persons", (req, res) => {
+  res.json(data);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const person = data.find((person) => person.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.statusMessage = "Person not found";
+    res.status(404).end();
+  }
+});
+
+// post
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+  const max = 1000000;
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: "Required information missing.",
+    });
+  }
+
+  if (data.find((person) => person.name === body.name)) {
+    return res.status(400).json({
+      error: "Name must be unique.",
+    });
+  }
+
+  const person = {
+    id: Math.floor(Math.random() * max).toString(),
+    name: body.name,
+    number: body.number,
+  };
+
+  data = data.concat(person);
+  res.json(person);
+});
+
+// delete
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  data = data.filter((person) => person.id !== id);
+  res.status(204).end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
